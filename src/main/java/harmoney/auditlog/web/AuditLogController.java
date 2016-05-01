0d896e91a91b2a2e88b6e907430e6e063844be61
donkhan <kamilkhan@rediffmail.com>
2016-05-01 12:25:55 +0530

    get logs method is added

--- src/main/java/harmoney/auditlog/web/AuditLogController.java
+++ src/main/java/harmoney/auditlog/web/AuditLogController.java
@@ -1,8 +1,15 @@
 package harmoney.auditlog.web;
 
 
+import harmoney.auditlog.model.AuditLog;
+import harmoney.auditlog.repository.AuditLogRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.core.Response;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +22,18 @@ public class AuditLogController {
     public Response index() {
     	String result = "This Micro Service will provide audit logs of the user activity in Money Box";
         return Response.ok().entity(result).header("Access-Control-Allow-Origin", "*")
+    			.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT").build();
+    }
+    
+    @Autowired
+	private AuditLogRepository auditLogRepository;
+    
+    @RequestMapping("/get-logs")
+    @CrossOrigin
+    public Response getLogs(){
+    	List<AuditLog> result = new ArrayList<AuditLog>();
+    	result = auditLogRepository.findAll();
+    	return Response.ok().entity(result).header("Access-Control-Allow-Origin", "*")
     			.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT").build();
     }
 
